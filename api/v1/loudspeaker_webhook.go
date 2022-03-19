be82de925836772ad68699cfa9a6e5de34842e6c
masanetes <masanetes@gmail.com>
2022-03-19 17:39:41 +0900

    initial commit

---
+++ api/v1/loudspeaker_webhook.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// log is for logging in this package.
+var loudspeakerlog = logf.Log.WithName("loudspeaker-resource")
+
+func (r *Loudspeaker) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+//+kubebuilder:webhook:path=/mutate-loudspeaker-masanetes-github-io-v1-loudspeaker,mutating=true,failurePolicy=fail,sideEffects=None,groups=loudspeaker.masanetes.github.io,resources=loudspeakers,verbs=create;update,versions=v1,name=mloudspeaker.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.Defaulter = &Loudspeaker{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (r *Loudspeaker) Default() {
+	loudspeakerlog.Info("default", "name", r.Name)
+
+	// TODO(user): fill in your defaulting logic.
+}
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+//+kubebuilder:webhook:path=/validate-loudspeaker-masanetes-github-io-v1-loudspeaker,mutating=false,failurePolicy=fail,sideEffects=None,groups=loudspeaker.masanetes.github.io,resources=loudspeakers,verbs=create;update,versions=v1,name=vloudspeaker.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.Validator = &Loudspeaker{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *Loudspeaker) ValidateCreate() error {
+	loudspeakerlog.Info("validate create", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object creation.
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *Loudspeaker) ValidateUpdate(old runtime.Object) error {
+	loudspeakerlog.Info("validate update", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object update.
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *Loudspeaker) ValidateDelete() error {
+	loudspeakerlog.Info("validate delete", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
